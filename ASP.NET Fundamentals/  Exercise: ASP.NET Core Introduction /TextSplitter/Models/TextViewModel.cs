2429da555d0608877aa9ba8187750b798f611aa7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-05-26 20:05:25 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/  Exercise: ASP.NET Core Introduction /TextSplitter/Models/TextViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitter.Models
+{
+    public class TextViewModel
+    {
+        [MinLength(2), MaxLength(30)]
+        [Required]
+        public string Text { get; set; }
+        public string SplitText { get; set; }
+    }
+}
