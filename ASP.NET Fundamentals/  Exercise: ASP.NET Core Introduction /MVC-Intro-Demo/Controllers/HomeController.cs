897eeafc2fd7d41463d04e2a8d7bc84b9ea85139
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-05-27 15:30:41 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/  Exercise: ASP.NET Core Introduction /MVC-Intro-Demo/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Diagnostics;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Home";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About() 
+        {
+            ViewBag.Message = "About";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NumbersToN()
+        {
+            ViewBag.Count = -1;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NumbersToN(int count = -1)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/  Exercise: ASP.NET Core Introduction /MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using MVC_Intro_Demo.Models;
+using System.Text;
+using static MVC_Intro_Demo.Seeding.DataProvider;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult All()
+        {
+            return View(Products);
+        }
+
+        public IActionResult ById(string id)
+        {
+            ProductViewModel? product = Products.FirstOrDefault(p => p.Id.ToString().Equals(id));
+
+            if(product == null)
+            {
+                return RedirectToAction("All");
+            }
+
+            return View(product);
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in Products)
+            {
+                sb.AppendLine($"{item.Name} {item.Price} lv.");
+            }
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.text");
+            return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+        }
+    }
+}
