c09718bd0a126d63381ac685d4b5aceaa3edc8cf
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-05-27 15:31:31 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/  Exercise: ASP.NET Core Introduction /MVC-Intro-Demo/Seeding/DataProvider.cs
@@ -0,0 +1,30 @@
+﻿using MVC_Intro_Demo.Models;
+
+namespace MVC_Intro_Demo.Seeding
+{
+    public static class DataProvider
+    {
+        public static IEnumerable<ProductViewModel> Products
+            = new List<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Cheese",
+                    Price = 10.20m,
+                },
+                new ProductViewModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Ham",
+                    Price = 5.50m,
+                },
+                new ProductViewModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Bread",
+                    Price = 2.20m,
+                },
+            };
+    }
+}
