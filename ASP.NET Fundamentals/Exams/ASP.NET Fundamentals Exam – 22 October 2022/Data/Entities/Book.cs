58ae6fe1cffc77e97c161e117c0f7b23d5de80f6
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-07 16:34:27 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exams/ASP.NET Fundamentals Exam – 22 October 2022/Data/Entities/Book.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Library.Constants.Constants.BookCosntants;
+
+namespace Library.Data.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(AuthorMaxLength)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Range(0.00, 10.00)]
+        public decimal Rating { get; set; }
+
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+        public IEnumerable<ApplicationUserBook> ApplicationUsersBooks { get; set; } 
+            = new List<ApplicationUserBook>();
+    }
+}
