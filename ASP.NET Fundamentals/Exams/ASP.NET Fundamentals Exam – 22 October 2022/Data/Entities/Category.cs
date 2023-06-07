da8878e37b90651da6a9007d4e61885dc6589b95
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-07 16:33:58 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exams/ASP.NET Fundamentals Exam – 22 October 2022/Data/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Library.Constants.Constants.CategoryConstants;
+
+namespace Library.Data.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+            = new List<Book>();
+    }
+}
