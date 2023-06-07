cea22ae120d982dd906af9854ef78cf559c525b3
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-07 16:36:18 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exams/ASP.NET Fundamentals Exam – 22 October 2022/Models/User/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Library.Constants.Constants.LoginConstants;
+
+namespace Library.Models.User
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(EmailNameMaxLength, MinimumLength = EmailMinLength)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(PasswordNameMaxLength, MinimumLength = PasswordMinLength)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
