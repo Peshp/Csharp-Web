4d58ddf61522c8c79a4a79ed30f6f2f8dd393723
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-07 16:34:48 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exams/ASP.NET Fundamentals Exam – 22 October 2022/Data/Entities/ApplicationUserBook.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Library.Data.Entities
+{
+    public class ApplicationUserBook
+    {
+        [Required]
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
