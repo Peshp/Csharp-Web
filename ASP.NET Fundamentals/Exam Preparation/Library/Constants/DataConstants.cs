08ca41c28c79fdcae5fe1a85a4aea3a04556f1a8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-11 16:03:41 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Constants/DataConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.Constants
+{
+    public class DataConstants
+    {
+        public static class BookConstants
+        {
+            public const int TitleMaxLength = 50;
+            public const int AuthorMaxLength = 50;
+            public const int DescriptionMaxLength = 5000;
+        }
+
+        public static class CategoryConstants
+        {
+            public const int NameMaxLength = 50;
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Constants/ModelConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Constants
+{
+    public class ModelConstants
+    {
+        public static class BookConstants
+        {
+            public const int TitleMaxLength = 50;
+            public const int TitleMinLength = 10;
+
+            public const int AuthorMaxLength = 50;
+            public const int AuthorMinLength = 5;
+
+            public const int DescriptionMaxLength = 5000;
+            public const int DescriptionMinLength = 5;
+        }
+
+        public static class CategoryConstants 
+        {
+            public const int NameMaxLength = 50;
+            public const int NameMinLength = 5;
+        }
+    }
+}
