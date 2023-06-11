d91e5741043a792bfa259ed849033bc6c3d9caa1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-11 16:04:26 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Interfaces/IBookService.cs
@@ -0,0 +1,23 @@
+﻿using Library.Models;
+
+namespace Library.Interfaces
+{
+    public interface IBookService
+    {
+        public Task<IEnumerable<BookViewModel>> GetBooksAsync();
+
+        public Task AddToMyCollection(string userId, int bookId);
+
+        public Task<IEnumerable<BookViewModel>> GetCurrentUserBooksCollectionAsync(string userId);
+
+        public Task<IEnumerable<CategoryFormModel>> GetAllCategories();
+
+        public Task AddNewBook(BookFormModel formModel);
+
+        public Task RemoveFromMyCollection(string userId, int bookId);
+
+        public Task EditBookAsync(BookEditModel formModel, int bookId);
+
+        public Task FindBookById(int id);
+    }
+}
