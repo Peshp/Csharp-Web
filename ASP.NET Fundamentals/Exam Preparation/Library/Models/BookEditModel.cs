d16a4c83e751fdc0ea6fe811ed85fee9130e8224
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-11 16:04:42 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Models/BookEditModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Library.Constants.ModelConstants.BookConstants;
+
+namespace Library.Models
+{
+    public class BookEditModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(AuthorMaxLength, MinimumLength = AuthorMinLength)]
+        public string Author { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0.00", "10.00")]
+        public decimal Rating { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryFormModel> Categories { get; set; }
+    }
+}
---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Models/CategoryFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class CategoryFormModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
