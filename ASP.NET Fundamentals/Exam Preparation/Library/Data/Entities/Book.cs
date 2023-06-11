f3b0978fc527f880de3587eb7596b2794759cc71
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-11 16:04:13 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Data/Entities/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Library.Constants.DataConstants.BookConstants;
+
+namespace Library.Data.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(AuthorMaxLength)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+        public ICollection<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
+    }
+}
---
+++ ASP.NET Fundamentals/Exam Preparation/Library/Data/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Library.Constants.DataConstants.CategoryConstants;
+
+namespace Library.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
